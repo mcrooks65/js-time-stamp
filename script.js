6688e14572e5d7cb7dc23ef98c517ef1f4ba702d
mcrooks65 <mfcrooks@gmail.com>
2021-12-16 18:04:04 -0800

    Add showCurrentTime function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("script.js is linked to index.html")
+var showCurrentTime = function()
+{
+    var clock = document.getElementById("clock");
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+
+    var minutes = currentTime.getMinutes();
+    if (minutes < 10)
+    {
+        minutes = "0" + minutes;
+    }
+
+    var seconds = currentTime.getSeconds();
+    if (seconds < 10)
+    {
+        seconds = "0" + seconds;
+    }
+
+    var clockTime = hours + ':' + minutes + ':' + seconds;
+
+    clock.innerText = clockTime;
+}
+
+document.onload = showCurrentTime();
